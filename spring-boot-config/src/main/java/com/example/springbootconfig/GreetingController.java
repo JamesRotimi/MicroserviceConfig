3b8b9de1aed34db6390ae69a604e0b0ada030a0d
jamesrotimi <jamesrotimi@icloud.com>
2021-01-11 22:10:01 +0000

    converting a app property files to a yml file - adding actuator to pom file

--- spring-boot-config/src/main/java/com/example/springbootconfig/GreetingController.java
+++ spring-boot-config/src/main/java/com/example/springbootconfig/GreetingController.java
@@ -1,8 +1,11 @@
 package com.example.springbootconfig;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class GreetingController {
@@ -10,8 +13,21 @@ public class GreetingController {
     @Value("${my.greeting}")
     private String greetingMessage;
 
+//    @Value("${some static message}")
+//    private String staticMessage;
+
+    @Value("${my.list.values}")
+    private List <String> listValues;
+
+    @Autowired
+    private DbSettings dbSettings;
+
+    public GreetingController() {
+
+    }
+
     @GetMapping("/greeting")
     public String greeting() {
-        return greetingMessage;
+        return dbSettings.getConnection() + dbSettings.getHost();
     }
 }
